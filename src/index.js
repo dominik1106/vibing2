fe818f73a40910a8d05d2c090a7458790e4129db
Dominik Schneider <dominik.p.s@outlook.de>
2025-02-21 13:19:17 +0100

    moved index.js to src folder

---
+++ src/index.js
@@ -0,0 +1,93 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { DisTube, DisTubeVoice, DisTubeVoiceManager } = require("distube");
+const { YouTubePlugin } = require("@distube/youtube");
+
+const { token } = require('../config.json');
+
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates,
+    ]
+});
+
+const distube = new DisTube(client, {
+  plugins: [new YouTubePlugin()],
+  joinNewVoiceChannel: true,
+});
+
+distube.on("error", (error, queue, song) => {
+    console.log(`[Error]: ${error.message}`);
+});
+
+distube.on("debug", (debug) => {
+    console.log(`[Debug]: ${debug}`);
+});
+
+distube.on("finish", (queue) => {
+    console.log(`[Finish]: ${queue}`);
+})
+
+distube.on("deleteQueue", (queue) => {
+    console.log(`[DeleteQueue]: ${queue}`);
+})
+
+async function joinChannel(channelId, song) {
+    const channel = await client.channels.fetch(channelId);
+    if(!channel) {
+        console.error("Did not find channel!");
+        return;
+    }
+
+    await distube.voices.join(channel);
+}
+
+async function addSong(guildId, channelId = null, song) {
+    let channel = null;
+    if(!channelId) {
+        channel = distube.voices.get(guildId).channel;
+        if(!channel && !channelId) {
+            console.error("Bot not connected to voice channel!");
+            return;
+        }
+    } else {
+        channel = await client.channels.fetch(channelId);
+    }
+
+    if(!channel) {
+        console.error("Did not find channel!");
+        return;
+    }
+
+    await distube.play(channel, song, {
+        skip: false,
+    });
+}
+
+async function togglePause(guildId) {
+    const queue = distube.getQueue(guildId);
+    if(!queue) {
+        console.error("Queue not found!");
+        return;
+    }
+
+    if(queue.paused) {
+        queue.resume();
+    } else {
+        queue.pause();
+    }
+}
+
+async function skip(guildId) {
+    try {
+        await distube.skip(guildId);
+    } catch(error) {
+        if(error.code === "NO_UP_NEXT") {
+            await distube.stop(guildId);
+        }
+    }
+}
+
+client.login(token).then(() => {
+    console.log(`Bot ready! Logged in as ${client.user.tag}`);
+});
