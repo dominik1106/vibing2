1e3f683dd8a6ef85b5f91f1e7085c61448520763
Dominik Schneider <dominik.p.s@outlook.de>
2025-02-26 17:10:14 +0100

    fix module export in auth route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -78,4 +78,4 @@ function isAuthenticated(req, res, next) {
     res.redirect('/login');
 }
 
-module.exports = { router, isAuthenticated };
+module.exports = { authRoutes: router, isAuthenticated };
